285c9f38610dbc98cc54d624bdf42a623a23648e
Kyle Davis <validkeys@gmail.com>
2015-06-17 09:22:16 -0400

    Installed ember-cli-document-title and created the title method in the application route

--- app/routes/people.js
+++ app/routes/people.js
@@ -2,6 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
+  // the in the application route is a function
+  // that receives the bubbled tokens from child routes
+  title: function(tokens) {
+    let base = 'AcmeCo';
+    return base;
+  },
+
   model: function() {
     return $.getJSON('/people.json').then((res) => {
       this.store.pushPayload('person', res);
