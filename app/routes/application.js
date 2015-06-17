bfb676d2bc20cd9493eb94b1fc061e7706af0086
Kyle Davis <validkeys@gmail.com>
2015-06-16 20:24:16 -0400

    Added the activate hook to the application route

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,3 +1,10 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({});
+export default Ember.Route.extend({
+
+  // start here by adding a document title
+  activate: function() {
+    document.title = "AcmeCo.";
+  }
+
+});
