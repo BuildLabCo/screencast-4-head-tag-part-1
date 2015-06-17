09a58a91d7b01bf6a922999c32286b28cb53a436
Kyle Davis <validkeys@gmail.com>
2015-06-17 08:18:34 -0400

    Created the title property and console log out a new path

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,10 +2,29 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
+  // First: "Add the title for this route"
+  title: "AcmeCo",
+
 
   actions: {
+
     didTransition: function() {
-      console.log("Setting the document title!");
+      // Second: grab the routes for all active leafs
+      let handlers = this.router.router.currentHandlerInfos;
+
+      // Third: Show that we can get the title for this route in the console
+      // by: handlers[0].handler.title
+
+      // Fourth: Add the title to the people route
+      // Fifth: Now we want to create a tokenized path
+      let path = _.chain(handlers)
+        .pluck('handler.title')
+        .compact()
+        .value()
+
+      // Six: Console log out so the user can see
+      console.log(path);  
+
     }
   }
 
--- app/routes/people.js
+++ app/routes/people.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
+  title: "People",
+
   model: function() {
     return $.getJSON('/people.json').then((res) => {
       this.store.pushPayload('person', res);
